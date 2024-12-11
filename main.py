2e36442a7b2aaf2ac186beb66fff789334d6b0a5
Mail-ish <ismailshah.imanshah@gmail.com>
2024-12-11 11:25:18 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from Controller import Controller
-
-if __name__ == "__main__":
-    # Create a controller instance
-    controller = Controller()
-    
-    # Run the Tic Tac Toe game
-    controller.run()
